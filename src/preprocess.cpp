f82d51ed7cd9552ac4472337d911720998775f7f
jiejie567 <498530205@qq.com>
2023-03-31 09:35:35 +0800

    for velodyne

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -168,28 +168,93 @@ void Preprocess::velodyne_handler(
   pl_full.clear();
 
   pcl::PointCloud<velodyne_ros::Point> pl_orig;
-  pcl::fromROSMsg(*msg, pl_orig);
-  int plsize = pl_orig.points.size();
-  // pl_surf.reserve(plsize);
-  for (int i = 0; i < pl_orig.size(); i++) {
-    PointType added_pt;
-    added_pt.x = pl_orig.points[i].x;
-    added_pt.y = pl_orig.points[i].y;
-    added_pt.z = pl_orig.points[i].z;
-    added_pt.intensity = pl_orig.points[i].intensity;
-    float angle = atan(added_pt.z / sqrt(added_pt.x * added_pt.x +
-                                         added_pt.y * added_pt.y)) *
-                  180 / M_PI;
-    int scanID = 0;
-    if (angle >= -8.83)
-      scanID = int((2 - angle) * 3.0 + 0.5);
-    else
-      scanID = N_SCANS / 2 + int((-8.83 - angle) * 2.0 + 0.5);
+    pcl::fromROSMsg(*msg, pl_orig);
+    int plsize = pl_orig.points.size();
+    if (plsize == 0) return;
+    pl_surf.reserve(plsize);
 
-    // use [0 50]  > 50 remove outlies
-    if (angle > 2 || angle < -24.33 || scanID > 50 || scanID < 0) {
-      continue;
+    /*** These variables only works when no point timestamps given ***/
+    double omega_l = 0.361 * 10;       // scan angular velocity
+    std::vector<bool> is_first(N_SCANS,true);
+    std::vector<double> yaw_fp(N_SCANS, 0.0);      // yaw of first scan point
+    std::vector<float> yaw_last(N_SCANS, 0.0);   // yaw of last scan point
+    std::vector<float> time_last(N_SCANS, 0.0);  // last offset time
+    /*****************************************************************/
+
+    if (pl_orig.points[plsize - 1].time > 0)
+    {
+      given_offset_time = true;
     }
-    pl_surf.push_back(added_pt);
-  }
+    else
+    {
+      given_offset_time = false;
+      double yaw_first = atan2(pl_orig.points[0].y, pl_orig.points[0].x) * 57.29578;
+      double yaw_end  = yaw_first;
+      int layer_first = pl_orig.points[0].ring;
+      for (uint i = plsize - 1; i > 0; i--)
+      {
+        if (pl_orig.points[i].ring == layer_first)
+        {
+          yaw_end = atan2(pl_orig.points[i].y, pl_orig.points[i].x) * 57.29578;
+          break;
+        }
+      }
+    }
+
+
+      for (int i = 0; i < plsize; i++)
+      {
+        PointType added_pt;
+        // cout<<"!!!!!!"<<i<<" "<<plsize<<endl;
+        
+        added_pt.normal_x = 0;
+        added_pt.normal_y = 0;
+        added_pt.normal_z = 0;
+        added_pt.x = pl_orig.points[i].x;
+        added_pt.y = pl_orig.points[i].y;
+        added_pt.z = pl_orig.points[i].z;
+        added_pt.intensity = pl_orig.points[i].intensity;
+        added_pt.curvature = pl_orig.points[i].time ;  // curvature unit: ms // cout<<added_pt.curvature<<endl;
+
+        if (!given_offset_time)
+        {
+          int layer = pl_orig.points[i].ring;
+          double yaw_angle = atan2(added_pt.y, added_pt.x) * 57.2957;
+
+          if (is_first[layer])
+          {
+            // printf("layer: %d; is first: %d", layer, is_first[layer]);
+              yaw_fp[layer]=yaw_angle;
+              is_first[layer]=false;
+              added_pt.curvature = 0.0;
+              yaw_last[layer]=yaw_angle;
+              time_last[layer]=added_pt.curvature;
+              continue;
+          }
+
+          // compute offset time
+          if (yaw_angle <= yaw_fp[layer])
+          {
+            added_pt.curvature = (yaw_fp[layer]-yaw_angle) / omega_l;
+          }
+          else
+          {
+            added_pt.curvature = (yaw_fp[layer]-yaw_angle+360.0) / omega_l;
+          }
+
+          if (added_pt.curvature < time_last[layer])  added_pt.curvature+=360.0/omega_l;
+
+          yaw_last[layer] = yaw_angle;
+          time_last[layer]=added_pt.curvature;
+        }
+
+        if (i % point_filter_num == 0)
+        {
+          if(added_pt.x*added_pt.x+added_pt.y*added_pt.y+added_pt.z*added_pt.z > (blind * blind))
+          {
+            pl_surf.points.push_back(added_pt);
+          }
+        }
+      }
+    
 }
